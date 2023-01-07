39354ef30bffec1824f3593013b4d028e8eb4e2c
Daniele Salerno <72736111+DanieleSalerno@users.noreply.github.com>
2023-01-07 14:32:04 +0100

    fixing spawn.py - set the initial pose manually

--- object_recognition/scripts/spawn.py
+++ object_recognition/scripts/spawn.py
@@ -4,10 +4,19 @@ from geometry_msgs.msg import Pose
 rospy.init_node('insert_object',log_level=rospy.INFO)
 print(rospy.INFO)
 
+poseSpawn=Pose()
+poseSpawn.position.x=1
+poseSpawn.position.y=1
+poseSpawn.position.z=0
+
+poseSpawn.orientation.x=0
+poseSpawn.orientation.y=0
+poseSpawn.orientation.z=0
+
 spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-spawn_model_client(model_name='prova3',
+spawn_model_client(model_name='lattina1',
     model_xml=open('/home/dan/catkin_ws/src/project_gazebo/worlds/sprite/model.sdf', 'r').read(),
     robot_namespace='/foo',
-    initial_pose=Pose(),
+    initial_pose=poseSpawn,
     reference_frame='world'
 )
